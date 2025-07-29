11b887ec56dfad235a5ba78f6a22ded19bea45d4
WU JAN AN <wl00262188@gmail.com>
2025-07-29 11:38:06 +0800

    Update clean_cache.sh

--- clean_cache.sh
+++ clean_cache.sh
@@ -1,7 +1,7 @@
-cache_dir=$(grep proxy_cache_path /usr/local/openresty/nginx/conf/nginx.conf | awk '{print $2}' | tr -s '/' )
-if [[ "$cache_dir" == "" || "$cache_dir" == "/"  ]];then
-    echo "无法获取缓存目录"
-else 
-   rm -rf ${cache_dir}/*
-fi
+CACHE_DIR="/data/nginx/cache/"
 
+if [[ -z "$CACHE_DIR" || "$CACHE_DIR" == "/" ]]; then
+  echo "无法获取缓存目录"
+else
+  rm -rf $CACHE_DIR/*
+fi
